b9eb5a143e3b80ffd142a304a5c6f4aa7f9a14fe
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2025-04-05 19:32:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html", // Include all HTML files in the public folder
+    "./src/**/*.js",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
